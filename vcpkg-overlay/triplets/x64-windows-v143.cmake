b3cdb989bd724d4b1cd2ddc195bdd861a6285a7e
sumof2primes <sumof2primes@gmail.com>
2023-11-17 18:49:56 +0900

    build: Add vcpkg-overlay
    llvm port, toolchains, triplets

---
+++ vcpkg-overlay/triplets/x64-windows-v143.cmake
@@ -0,0 +1,13 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_PLATFORM_TOOLSET "v143")
+set(VCPKG_DEP_INFO_OVERRIDE_VARS "v143")
+set(VCPKG_BUILD_TYPE release)
+
+# Toolchain setup
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../scripts/toolchains/windows.cmake")
+
+# Setting VCPKG_CHAINLOAD_TOOLCHAIN_FILE deactivates automatic vcvars setup so reenable it!
+set(VCPKG_LOAD_VCVARS_ENV ON)
+
+# Port specialization
+include("${CMAKE_CURRENT_LIST_DIR}/windows/port.cmake")
