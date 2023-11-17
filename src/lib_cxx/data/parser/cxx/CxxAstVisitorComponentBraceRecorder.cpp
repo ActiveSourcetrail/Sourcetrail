491d3872337ee600bcb7a906e53ef54d66026872
sumof2primes <sumof2primes@gmail.com>
2023-11-17 19:21:31 +0900

    src: Migrate to LLVM 16
    Use CLANG_VERSION_MAJOR Compiler Directive switch

--- src/lib_cxx/data/parser/cxx/CxxAstVisitorComponentBraceRecorder.cpp
+++ src/lib_cxx/data/parser/cxx/CxxAstVisitorComponentBraceRecorder.cpp
@@ -154,13 +154,13 @@ clang::SourceLocation CxxAstVisitorComponentBraceRecorder::getFirstLBraceLocatio
 	while (true)
 	{
 		llvm::Optional<clang::Token> token = clang::Lexer::findNextToken(searchStartLoc, sm, opts);
-		if (token.hasValue())
+		if (token)
 		{
-			if (token.getValue().getKind() == clang::tok::l_brace)
+			if (token->getKind() == clang::tok::l_brace)
 			{
-				return token.getValue().getLocation();
+				return token->getLocation();
 			}
-			searchStartLoc = token.getValue().getLocation();
+			searchStartLoc = token->getLocation();
 		}
 		else
 		{
@@ -187,9 +187,9 @@ clang::SourceLocation CxxAstVisitorComponentBraceRecorder::getLastRBraceLocation
 	{
 		searchEndLoc = searchEndLoc.getLocWithOffset(-1);
 		llvm::Optional<clang::Token> token = clang::Lexer::findNextToken(searchEndLoc, sm, opts);
-		if (token.hasValue() && token.getValue().getKind() == clang::tok::r_brace)
+		if (token && token->getKind() == clang::tok::r_brace)
 		{
-			return token.getValue().getLocation();
+			return token->getLocation();
 		}
 	}
 
